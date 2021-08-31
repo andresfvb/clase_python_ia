17865feacb49f21642ea2192724c9aa9e31009ba
andresfvb <barandicandres@gmail.com>
2021-08-30 20:55:10 -0500

    Ejercicios y captura por pantalla

--- Desktop/clase_python_ia/clase_uno.py
+++ Desktop/clase_python_ia/clase_uno.py
@@ -18,7 +18,18 @@ y = str(a)
 print(y)
 print(type(y))
 
-#Concatenaciones
-a = "Hola"
-b = "Mundo"
-c = a + ' ' + b
+# capturar por pantalla
+
+nombre = input('Digite su nombre: ')
+print('Hola',nombre)
+
+print('Digite su nombre: ')
+nombre = input()
+print('Hola',nombre)
+
+# SUMA DOS NUMERO E IMPRIMA RESULTADO
+
+n1 = float(input ('digita el primer numero: '))
+n2 = float(input ('digita el segundo numero: '))
+nr = n1 + n2
+print('La suma de',n1,'+',n2,'es',nr)
