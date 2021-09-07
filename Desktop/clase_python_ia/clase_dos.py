4e210fd7ef9d5df5b2f49735d573f957b98f9ebd
andresfvb <barandicandres@gmail.com>
2021-09-06 19:38:18 -0500

    condicionales

---
+++ Desktop/clase_python_ia/clase_dos.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 19:26:14 2021
+
+@author: Andres Vasquez
+"""
+
+# Condicionales
+
+x = 2
+if(x > 0):
+    print('1')
+else:
+    print('2')
+    print('3')
+
+# Algoritmo que dada la eda diga si es mayor o menor de endad
+
+edad = int((input("Digite la edad de la persona: "))
+if(edad >= 18):
+    print("La persona es mayor de edad")
+else:
+    print("La persona es menor de edad")
--- Desktop/clase_python_ia/clase_uno.py
+++ Desktop/clase_python_ia/clase_uno.py
@@ -21,15 +21,34 @@ print(type(y))
 # capturar por pantalla
 
 nombre = input('Digite su nombre: ')
-print('Hola',nombre)
+print('Hola', nombre)
 
 print('Digite su nombre: ')
 nombre = input()
-print('Hola',nombre)
+print('Hola', nombre)
 
 # SUMA DOS NUMERO E IMPRIMA RESULTADO
 
-n1 = float(input ('digita el primer numero: '))
-n2 = float(input ('digita el segundo numero: '))
+n1 = float(input('digita el primer numero: '))
+n2 = float(input('digita el segundo numero: '))
 nr = n1 + n2
-print('La suma de',n1,'+',n2,'es',nr)
+
+# print('La suma de',n1,'+',n2,'es',nr)
+
+print('La suma de los numeros {n1} + {n2} es {suma}')
+
+# Haga un numero y eleve al cuadrado
+
+numero_uno = float(input('Digita el numero a elevar: '))
+numero_resultante = pow(numero_uno, 2)
+print(f'La potencia de {numero_uno} es {numero_resultante}')
+
+# HAGA QUE TOME EL VALOR DE UN PRODUCTO Y APLIQUE EL 20%
+
+numero_uno = float(input('Digita el valor del producto: $'))
+descuento = numero_uno * 0.20
+numero_resultante = numero_uno - descuento
+print(f'Valor: ${numero_uno:,}')
+print(f'Descuento del 20%: ${descuento:,}')
+print(f'Valor final: ${numero_resultante:,}')
+
